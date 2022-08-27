63cc616b751c4e7d16e10db7de69c5c9ade42a51
RaccoonCoder7 <RaccoonCoder7@gmail.com>
2022-08-27 15:22:21 +0900

    - 게임씬 구조 수정 - 로프이동/로프점프 구현

--- SMG2208/Assets/04_Scripts/PlayerMgr.cs
+++ SMG2208/Assets/04_Scripts/PlayerMgr.cs
@@ -7,8 +7,13 @@ public class PlayerMgr : SingletonMono<PlayerMgr>
     public int hp;
     public PlayerState playerState;
     public float jumpPower;
+    public Transform bulbTr;
+    public int lerpSpeed;
 
     private Rigidbody2D rigid;
+    private Coroutine detectBulbRoutine;
+    private Coroutine moveToBulbRoutine;
+    private float originGravityScale;
 
     public enum PlayerState
     {
@@ -23,6 +28,7 @@ public class PlayerMgr : SingletonMono<PlayerMgr>
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        originGravityScale = rigid.gravityScale;
     }
 
     void Update()
@@ -75,30 +81,76 @@ public class PlayerMgr : SingletonMono<PlayerMgr>
 
     private void Jump()
     {
-        rigid.AddForce(Vector3.up *jumpPower, ForceMode2D.Impulse);
+        rigid.AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
         playerState = PlayerState.Jump;
     }
 
     private void DetectBulb()
     {
-        // TODO: 로프 발사
+        if (detectBulbRoutine != null) return;
+
+        detectBulbRoutine = StartCoroutine(DetectBulbRoutine());
     }
 
     private void CancelDetectBulb()
     {
-        // TODO: 로프 발사 취소
+        if (detectBulbRoutine != null)
+        {
+            StopCoroutine(detectBulbRoutine);
+            detectBulbRoutine = null;
+        }
+
+        detectBulbRoutine = null;
         playerState = PlayerState.None;
     }
 
     private void MoveToBulb()
     {
-        // TODO: Y값 변경
+        if (moveToBulbRoutine != null) return;
+
+        moveToBulbRoutine = StartCoroutine(MoveToBulbRoutine());
     }
 
     private void CancelMoveToBulb()
     {
-        // TODO: Y값 변경 취소
+        if (moveToBulbRoutine != null)
+        {
+            StopCoroutine(moveToBulbRoutine);
+            moveToBulbRoutine = null;
+        }
+
+        rigid.gravityScale = originGravityScale;
         playerState = PlayerState.None;
+    }
+
+    private IEnumerator DetectBulbRoutine()
+    {
+        // TODO: 로프 발사 구현
+        yield return new WaitForSeconds(0.1f);
+        // TODO: bulb 오브젝트 세팅
+
+        playerState = PlayerState.RopeJump;
+        detectBulbRoutine = null;
+    }
+
+    private IEnumerator MoveToBulbRoutine()
+    {
+        rigid.gravityScale = 0;
+        rigid.velocity = Vector2.zero;
+        Vector3 targetPos = new Vector3(transform.position.x, bulbTr.position.y, 0);
+
+        // TODO: 타겟의 x값을 가지고 lerpSpeed를 바꿔줘야 함
+        while (targetPos.y * 0.9f > transform.position.y)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * lerpSpeed);
+            yield return null;
+        }
+
+        rigid.gravityScale = originGravityScale;
+        rigid.velocity = Vector2.zero;
+        rigid.AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
+        playerState = PlayerState.None;
+        moveToBulbRoutine = null;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
