a17f9c3db07339be7855f962d7c6ba2a64532124
RaccoonCoder7 <RaccoonCoder7@gmail.com>
2022-08-28 01:01:05 +0900

    - 게임씬 UI 작어 완료 - Scroller 버그 수정

--- SMG2208/Assets/04_Scripts/Scroller.cs
+++ SMG2208/Assets/04_Scripts/Scroller.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scroller : MonoBehaviour
 {
     public List<RectTransform> targetRectTrList = new List<RectTransform>();
     public float scrollSpeed;
+
+    private float screenWidth;
+
+    void Start()
+    {
+        screenWidth = GetComponent<CanvasScaler>().referenceResolution.x;
+        for (int i = 1; i < targetRectTrList.Count; i++)
+        {
+            var pos = targetRectTrList[i].localPosition;
+            pos.x = screenWidth * i;
+            targetRectTrList[i].localPosition = pos;
+        }
+    }
 
     void Update()
     {
@@ -14,9 +28,9 @@ public class Scroller : MonoBehaviour
         foreach (var rect in targetRectTrList)
         {
             var pos = rect.localPosition;
-            if (pos.x <= -Screen.width)
+            if (pos.x <= -screenWidth)
             {
-                pos.x += Screen.width * targetRectTrList.Count;
+                pos.x += screenWidth * targetRectTrList.Count;
             }
             pos.x -= Time.deltaTime * scrollSpeed * 100;
             rect.localPosition = pos;
