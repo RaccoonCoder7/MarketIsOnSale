cf8fd747b68b4847ed56bf9e22fd171c24d5456e
Mitaro <jhkim9622@gmail.com>
2022-08-27 18:19:34 +0900

    게임 매니저 수정

--- SMG2208/Assets/04_Scripts/GameMgr.cs
+++ SMG2208/Assets/04_Scripts/GameMgr.cs
@@ -1,25 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameMgr : SingletonMono<GameMgr>
 {
     public int score;
     public float totalTime;
-    public GameState gameState;
-    public int scorePerSecond;
-    public int scorePerItem;
+    public GameState gameState = GameState.None;
+    public int scorePerSecond = 100;
+    public int scorePerItem = 500;
     public int gameLimitTime = 120;
     public int totalItemCnt;
-    public GameObject menuSet;
-    public GameObject howToPlayWindow;
-    public GameObject timeBoard;
-    public GameObject scoreBoard;
-    public GameObject countdown;
 
     private float tempTime;
-    private float count;
 
     public enum GameState
     {
@@ -28,49 +21,14 @@ public class GameMgr : SingletonMono<GameMgr>
         Pause
     }
 
-    void Start()
-    {
-        gameState = GameState.None;
-        howToPlayWindow.SetActive(true);
-        timeBoard.SetActive(false);
-        scoreBoard.SetActive(false);
-        countdown.SetActive(false);
-    }
-
     void Update()
     {
         switch (gameState)
         {
             case GameState.None:
-                break;
             case GameState.Pause:
-                // Press escape key to resume
-                if (Input.GetButtonDown("Cancel"))
-                {
-                    GameResume();
-                }
                 break;
             case GameState.Play:
-                // Countdown
-                if (countdown.activeSelf)
-                {
-                    Text countdownText = countdown.GetComponentInChildren<Text>();
-                    count -= Time.deltaTime;
-                    countdownText.text = ((int)count + 1).ToString();
-                    if (count <= 0.0f)
-                    {
-                        countdown.SetActive(false);
-                    }
-                    break;
-                }
-
-                // Press escape key to pause
-                if (Input.GetButtonDown("Cancel"))
-                {
-                    GamePause();
-                    break;
-                }
-
                 // Update the time and score
                 tempTime += Time.deltaTime;
                 totalTime += Time.deltaTime;
@@ -113,36 +71,5 @@ public class GameMgr : SingletonMono<GameMgr>
     public int GetTotalScore()
     {
         return (totalItemCnt * scorePerItem) + (gameLimitTime * scorePerSecond);
-    }
-
-    // Start the game
-    public void GameStart()
-    {
-        gameState = GameState.Play;
-        howToPlayWindow.SetActive(false);
-        timeBoard.SetActive(true);
-        scoreBoard.SetActive(true);
-        countdown.SetActive(true);
-        count = 3.0f;
-    }
-
-    // Pause the game
-    public void GamePause()
-    {
-        gameState = GameState.Pause;
-        menuSet.SetActive(true);
-    }
-
-    // Resume the game
-    public void GameResume()
-    {
-        gameState = GameState.Play;
-        menuSet.SetActive(false);
-    }
-
-    // Quit the game and back to main menu
-    public void GameExit()
-    {
-        ResetGame();
     }
 }
---
+++ SMG2208/Assets/04_Scripts/UIMgr.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMgr : MonoBehaviour
+{
+    public GameMgr.GameState gameState = GameMgr.In.gameState;
+    public GameObject menuSet;
+    public GameObject howToPlayWindow;
+    public GameObject timeBoard;
+    public GameObject scoreBoard;
+    public GameObject countdown;
+
+    private float count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameState = GameMgr.GameState.None;
+        menuSet.SetActive(false);
+        howToPlayWindow.SetActive(true);
+        timeBoard.SetActive(false);
+        scoreBoard.SetActive(false);
+        countdown.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (gameState)
+        {
+            case GameMgr.GameState.None:
+                // Countdown
+                if (countdown.activeSelf)
+                {
+                    Text countdownText = countdown.GetComponentInChildren<Text>();
+                    count -= Time.deltaTime;
+                    countdownText.text = ((int)count + 1).ToString();
+                    if(count <= 0.0f)
+                    {
+                        gameState = GameMgr.GameState.Play;
+                        countdown.SetActive(false);
+                    }
+                }
+                break;
+            case GameMgr.GameState.Pause:
+                // Press escape key to resume
+                if (Input.GetButtonDown("Cancel"))
+                {
+                    GameResume();
+                }
+                break;
+            case GameMgr.GameState.Play:
+                // Press escape key to pause
+                if (Input.GetButtonDown("Cancel"))
+                {
+                    GamePause();
+                }
+                break;
+        }
+    }
+
+    // Start the game
+    public void GameStart()
+    {
+        howToPlayWindow.SetActive(false);
+        timeBoard.SetActive(true);
+        scoreBoard.SetActive(true);
+        countdown.SetActive(true);
+        count = 3.0f;
+    }
+
+    // Pause the game
+    public void GamePause()
+    {
+        gameState = GameMgr.GameState.Pause;
+        menuSet.SetActive(true);
+    }
+
+    // Resume the game
+    public void GameResume()
+    {
+        gameState = GameMgr.GameState.Play;
+        menuSet.SetActive(false);
+    }
+
+    // Quit the game and back to main menu
+    public void GameExit()
+    {
+        GameMgr.In.ResetGame();
+    }
+}
