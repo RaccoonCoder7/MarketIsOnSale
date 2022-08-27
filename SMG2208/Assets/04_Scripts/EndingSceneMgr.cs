44b33b32dac1c917833f8c8df732060b424d6041
Mitaro <jhkim9622@gmail.com>
2022-08-28 08:31:34 +0900

    엔딩씬 수정

--- SMG2208/Assets/04_Scripts/EndingSceneMgr.cs
+++ SMG2208/Assets/04_Scripts/EndingSceneMgr.cs
@@ -11,6 +11,8 @@ public class EndingSceneMgr : MonoBehaviour
     public Text quote;
     public string GameSceneName;
     public string StartSceneName;
+    public Sprite GameOver;
+    public Sprite GameClear;
 
     // Start is called before the first frame update
     void Start()
@@ -18,15 +20,18 @@ public class EndingSceneMgr : MonoBehaviour
         Text scoreBoardText = scoreBoard.GetComponentInChildren<Text>();
         int totalScore = GameMgr.In.GetTotalScore();
         scoreBoardText.text = "점수: " + totalScore.ToString();
+        RectTransform rect = (RectTransform)portrait.transform;
         if (PlayerMgr.In.hp <= 0)
         {
-            portrait.enabled = true;
+            portrait.sprite = GameOver;
+            rect.sizeDelta = new Vector2(585, 700);
             talkerName.text = "아주머니";
             quote.text = "그러게 총각 빨리 오지 그랬어~";
         }
         else
         {
-            portrait.enabled = false;
+            portrait.sprite = GameClear;
+            rect.sizeDelta = new Vector2(500, 500);
             talkerName.text = "피치";
             quote.text = "이 정도면 충분하겠지? 이제 배송을 하러 가볼까?";
         }
