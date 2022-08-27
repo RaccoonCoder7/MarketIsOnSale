1d536d2f6ad101335bf8506317a994328d7c7e39
RaccoonCoder7 <RaccoonCoder7@gmail.com>
2022-08-28 01:25:16 +0900

    - 오디오매니저 생성

---
+++ SMG2208/Assets/04_Scripts/AudioMgr.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMgr : SingletonMono<AudioMgr>
+{
+    public List<AudioClip> clipList = new List<AudioClip>();
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayOneShot(int clipIndex)
+    {
+        audioSource.PlayOneShot(clipList[clipIndex]);
+    }
+
+    public void Play(int clipIndex)
+    {
+        audioSource.Stop();
+        audioSource.clip = clipList[clipIndex];
+        audioSource.Play();
+    }
+}
